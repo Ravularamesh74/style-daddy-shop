c92e05f4984dd12ff876b86e314456ead5d0355c
Ravula Ramesh <ravularamesh74@gmail.com>
2026-03-30 13:07:47 +0530

    feat: initialize React router and setup main entry point for client application

--- playwright-fixture.ts
+++ playwright-fixture.ts
@@ -1,3 +1,6 @@
 // Re-export the base fixture from the package
 // Override or extend test/expect here if needed
 export { test, expect } from "playwright-config/fixture";
+
+
+
--- playwright.config.ts
+++ playwright.config.ts
@@ -6,3 +6,5 @@ export default {
   //   baseURL: 'http://localhost:3000',
   // },
 };
+
+
